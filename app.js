bbffcdd955e1259d9d18c9ae9d0f22af46840d03
binteabid <abidsanaonly@gmail.com>
2025-03-12 06:21:59 +0500

    Chapter1,2,3)

---
+++ app.js
@@ -0,0 +1,171 @@
+
+
+document.write( "<h1>Assignment 1: Chapter 1 to 10</h1><hr>");
+// Chapter 1 Alerts!
+document.write("<h2>Chapter 1 Alerts!</h2><br>")
+
+/* 1. Write a script to greet your website visitor using JS alert
+box.*/
+
+alert("Welcome")
+
+/* 2. Write a script to display following message on your web
+Page*/
+
+alert("Error! Please enter a valid password.")
+
+/* 3. Write a script to display following message on your web
+page: (Hint : Use line break)*/
+
+alert("Welcome to JS Land...\n Happy Coding!")
+
+/* 4. Write a script to display following messages in sequence:*/
+
+alert("Welcome to JS Land..")
+
+
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+
+/*5. Generate the following message through browser's
+developer console:*/
+
+alert("Hello! I can run JS through my web browser's console")
+
+
+// The End
+
+
+// Chapter 2 Variables For Strings
+document.write("<h2> Chapter 2 Variables For Strings</h2><br>")
+
+
+// 1. Declare a variable called username.
+
+let username;
+
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+let myName = " Sana Abid"
+
+/*3. Write script to 
+a) Declare a JS variable, titled message.*/
+
+let message;
+
+
+// b) Assign "Hello World" to variable message
+
+message = "Hello World"
+
+// c) Display the message in alert box.
+
+alert(message)
+
+
+// 4. Write a script to save student's bio data in JS variables and show the data in alert boxes.
+
+// Student's Bio Data
+
+let studentName = "Ahmed Ali"
+alert(studentName)
+
+let studentAge = "15 years old"
+
+alert(studentAge)
+
+let studentCourse = "Certified Mobile Application Development"
+alert(studentCourse)
+
+// 5. Write a script to display the following alert using one JS variable:
+
+let jsVariable = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(jsVariable)
+
+/* 6. Declare a variable called email and assign to it a string that 
+represents your Email Address(e.g. example@example.com)
+Show the blow mentioned message in an alert box.(Hint: use
+string concatenation)*/
+
+let email = "abidsanaonly@gmail.com"
+alert("Email Address: " + email)
+
+
+/*7. Declare a variable called book & give it the value
+smarter way to learn JavaScript". Display the following
+message in an alert box:*/
+
+
+let book = "Smarter Way to Learn JavaScript"
+alert("I am trying to learn from the Book " + book)
+
+
+// 8. Write a script to display this in browser through JS
+
+document.write('<h2>Yah! I can write HTML content through JavaScript</h2><br>')
+
+// 9. Store following string in a variable and show in alert and browser through JS
+
+
+
+let string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(string)
+document.write(string+"<br>")
+
+
+// The End
+
+// Chapter #3: Variable for Numbers
+
+document.write("<h2>Chapter #3: Variable for Numbers</h2><br>")
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+let age;
+age= 15
+
+alert("I am " + age +"years old.")
+
+/* 2. Declare & initialize a variable to keep track of how many
+times a visitor has visited a web page. Show his/her
+number of visits on your web page. For example: "You
+have visited this site N times".  */
+
+let visitsNo=4
+
+alert("You have visited this site " + visitsNo + "times.")
+
+/* 3. Declare a variable called birthyear & assign to it your
+birth year. Show the following message in your browser:*/
+
+let birthyear;
+birthyear=1990
+
+document.write(  "My birth year is" + 1990 +  "<br> Data type of my declared variable is number.")
+
+
+/*  4. A visitor visits an online clothing store
+www.xyzClothing.com . Write a script to store in variables
+the following information:
+a. Visitor's name
+b. Product title
+c. Quantity i.e. how many products a visitor wants to
+order Show the following message in your browser: "John
+Doe ordered 5 T-shirt(s) on XYZ Clothing store".*/
+
+
+var visitorName = "John Doe"
+var productTitle = "T-shirt" 
+var quantity = 5
+
+document.write("<br>" + visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store." );
+
+document.write("<h2></h2><br>")
+document.write("<h2></h2><br>")
+document.write("<h2></h2><br>")
+document.write("<h2></h2><br>")
+document.write("<h2></h2><br>")
+document.write("<h2></h2><br>")
+document.write("<h2></h2><br>")
+document.write("<h2></h2><br>")
